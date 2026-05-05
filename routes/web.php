601df775cbe79a5c82eae4591da0f4d58996e705
ETHER-primer <y4s1r896@gmail.com>
2026-05-05 13:48:22 +0800

    Menambahkan fitur login dan role admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route yang harus login
+Route::middleware('auth')->group(function () {
+
+    // Tampilkan Data
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
+    // Update data
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    
+    // Delate khusus admin
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
